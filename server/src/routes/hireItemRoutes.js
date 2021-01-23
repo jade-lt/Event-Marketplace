f9f89295970e1ac42268b6d90473d11d0c0f0c45
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-01-23 11:58:29 +1100

    added pacth request to hire item routes, works ok and updates mongodb compass too

--- server/src/routes/hireItemRoutes.js
+++ server/src/routes/hireItemRoutes.js
@@ -8,14 +8,26 @@ router.get("/", (req, res) => res.send("this is the hire items router"));
 router.get("/test", (req, res) => res.send("test OK"));
 
 router.post("/new-hire-item", (req, res) => {
-    const reqBody = req.body;
-    HireItemModel.create(reqBody).then((data) => {
-        console.log(data);
-        res.send("item added successfully");
+  const reqBody = req.body;
+  HireItemModel.create(reqBody).then((data) => {
+    console.log(data);
+    res.send("item added successfully");
+  });
+});
+
+router.patch("/update-item/:id", (req, res) => {
+  HireItemModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    upsert: true,
+  })
+    .then((data) => {
+      console.log("item updated successfully");
+      res.send(data);
+    })
+    .catch(() => {
+      console.log("something went wrong");
+      res.status(404).send("this hire item doesn't exist");
     });
 });
 
-router.get("/my-items", (req, res) => res.send("this is where my items will be displayed"));
-
 module.exports = router;
- 
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.send("this is the user router"));
 
+router.get("/my-items", (req, res) => res.send("this is where my items will be displayed"));
+
 module.exports = router;
