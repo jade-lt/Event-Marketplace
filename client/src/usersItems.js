e4fd603efc319f5eeac55882f89a6ab36eb400bf
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2021-01-31 17:56:14 +1100

    users items now render to the dashboard on login

--- client/src/usersItems.js
+++ client/src/usersItems.js
@@ -1,20 +1,30 @@
-
+const usersItems = `
+<div id="item-list"></div>
+`;
 
 const displayUserItems = () => {
-    $.get(`http://localhost:3000/api/user/my-items`, (data) => {
+  $.get(`http://localhost:3000/api/user/my-items`, (data) => {
     console.log(data);
 
-    // const itemCategory = data[0].categoryId;
-    const itemId = data[0]._id;
-    const itemName = data[0].itemName;
-    const itemColor = data[0].itemColor;
-    const itemSize = data[0].dimensions;
-    const amount = data[0].numberAvailable;
-    const itemCost = data[0].costPerHire;
-    const itemAvailability = data[0].available;
+    const itemDataArray = Object.values(data);
 
-    console.log(itemName);
-    });
+    // console.log(itemData);
+
+    function returnItemData(arr) {
+      for (let i = 0; i < arr.length; i++) {
+        const itemData = arr[i];
+        console.log(itemData);
+        $("#item-list").append(
+          `<div class="user-items-list"><h5 class="user-item-names">${itemData.itemName}</h5><p>Colour: ${itemData.itemColor}<br>Size: ${itemData.dimensions}<br>Amount: ${itemData.numberAvailable}<br>Cost: ${itemData.costPerHire}<br>Currently Available for hire: ${itemData.available}<br>Category: ${itemData.categoryId}<br>Item ID: ${itemData._id}<br></p></div>`
+        );
+      }
+    }
+    // add to the function an html tag that appends to the right section of the items div
+
+    console.log(returnItemData(itemDataArray));
+
+  });
+  return usersItems;
 };
 
 export default displayUserItems;
